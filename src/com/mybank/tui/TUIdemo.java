f131f93e662f080c921f16e90df5131234d6d891
Roma-Cherny-15 <145929935+Roma-Cherny-15@users.noreply.github.com>
2024-06-18 12:57:51 +0300

    Completed

---
+++ src/com/mybank/tui/TUIdemo.java
@@ -0,0 +1,112 @@
+package com.mybank.tui;
+
+import jexer.TAction;
+import jexer.TApplication;
+import jexer.TField;
+import jexer.TText;
+import jexer.TWindow;
+import jexer.event.TMenuEvent;
+import jexer.menu.TMenu;
+import com.mybank.data.*;
+import com.mybank.domain.*;
+/**
+ *
+ * @author Alexander 'Taurus' Babich
+ */
+public class TUIdemo extends TApplication {
+
+    private static final int ABOUT_APP = 2000;
+    private static final int CUST_INFO = 2010;
+    private static String dataFilePath;
+    private static final String USAGE
+    = "USAGE: java com.mybank.tui.TUIdemo <dataFilePath>";
+    public static void main(String[] args) throws Exception {
+        if ( args.length != 1 ) {
+            System.out.println(USAGE);
+        } 
+        else {
+            dataFilePath = args[0];
+            TUIdemo tdemo = new TUIdemo();
+            (new Thread(tdemo)).start();
+        }
+        
+        
+    }
+
+    public TUIdemo() throws Exception {
+        super(BackendType.SWING);
+
+        addToolMenu();
+        //custom 'File' menu
+        TMenu fileMenu = addMenu("&File");
+        fileMenu.addItem(CUST_INFO, "&Customer Info");
+        fileMenu.addDefaultItem(TMenu.MID_SHELL);
+        fileMenu.addSeparator();
+        fileMenu.addDefaultItem(TMenu.MID_EXIT);
+        //end of 'File' menu  
+
+        addWindowMenu();
+
+        //custom 'Help' menu
+        TMenu helpMenu = addMenu("&Help");
+        helpMenu.addItem(ABOUT_APP, "&About...");
+        //end of 'Help' menu 
+
+        setFocusFollowsMouse(true);
+        //Customer window
+        
+        //String dataFilePath = "test.dat";
+        DataSource dataSource = new DataSource(dataFilePath);
+	dataSource.loadData();
+        ShowCustomerDetails();
+    }
+
+    @Override
+    protected boolean onMenu(TMenuEvent menu) {
+        if (menu.getId() == ABOUT_APP) {
+            messageBox("About", "\t\t\t\t\t   Just a simple Jexer demo.\n\nCopyright \u00A9 2019 Alexander \'Taurus\' Babich").show();
+            return true;
+        }
+        if (menu.getId() == CUST_INFO) {
+            ShowCustomerDetails();
+            return true;
+        }
+        return super.onMenu(menu);
+    }
+
+    private void ShowCustomerDetails() {
+        
+        
+        TWindow custWin = addWindow("Customer Window", 2, 1, 40, 10, TWindow.NOZOOMBOX);
+        custWin.newStatusBar("Enter valid customer number and press Show...");
+        
+        custWin.addLabel("Enter customer number: ", 2, 2);
+        TField custNo = custWin.addField(24, 2, 3, false);
+        TText details = custWin.addText("Owner Name: \nAccount Type: \nAccount Balance: ", 2, 4, 38, 8);
+        custWin.addButton("&Show", 28, 2, new TAction() {
+            @Override
+            public void DO() {
+                try {
+                    int custNum = Integer.parseInt(custNo.getText());
+                    Customer customer = Bank.getCustomer(custNum);
+                    String accountType;
+                    //details about customer with index==custNum
+                    if(customer.getAccount(0) instanceof SavingsAccount){
+                        accountType = "Saving";
+                    }
+                    else if(customer.getAccount(0) instanceof CheckingAccount) {
+                        accountType = "Checking";
+                    }
+                    else{
+                        accountType = "undefined";
+                    }
+                    details.setText("Owner Name: " + customer.getFirstName() + " " +
+                            customer.getLastName() + " (id="+custNum+")\nAccount Type: '"+ accountType
+                            + "'\nAccount Balance: " + customer.getAccount(0).getBalance() + "$");
+                } catch (Exception e) {
+                    messageBox("Error", "You must provide a valid customer number!").show();
+                }
+            }
+        });
+    }
+}
